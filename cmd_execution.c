f86c584a1b653cf271ef0de9291b7eaea3af3e29
Sally-Builds <uzoagulujoshua@yahoo.com>
2023-08-24 13:17:49 -0700

    command parse and execute

---
+++ cmd_execution.c
@@ -0,0 +1,61 @@
+#include "main.h"
+
+int parse_command(char *command)
+{
+    int i;
+    char *internal_command[] = {"env", "exit", NULL};
+    char *path = NULL;
+
+    for (i = 0; command[i] != '\0'; i++)
+    {
+        if (command[i] == '/')
+            return (EXTERNAL_COMMAND);
+    }
+    for (i = 0; internal_command[i] != NULL; i++)
+    {
+        if (_strcmp(command, internal_command[i]) == 0)
+            return (INTERNAL_COMMAND);
+    }
+    path = check_path(command);
+    if (path != NULL)
+    {
+        free(path);
+        return (PATH_COMMAND);
+    }
+    return (INVALID_COMMAND);
+}
+
+void execute_command(char **tokenized_command, int command_type)
+{
+    void (*func)(char **command);
+
+    if (command_type == EXTERNAL_COMMAND)
+    {
+        if (execve(tokenized_command[0], tokenized_command, NULL) == -1)
+        {
+            perror(_getenv("PWD"));
+            exit(2);
+        }
+    }
+    if (command_type == PATH_COMMAND)
+    {
+        if (execve(check_path(tokenized_command[0]), tokenized_command, NULL) == -1)
+        {
+            perror(_getenv("PWD"));
+            exit(2);
+        }
+    }
+    if (command_type == INTERNAL_COMMAND)
+    {
+        func = get_func(tokenized_command[0]);
+        func(tokenized_command);
+    }
+    if (command_type == INVALID_COMMAND)
+    {
+        print(g_shell_name, STDERR_FILENO);
+        print(": 1: ", STDERR_FILENO);
+        print(tokenized_command[0], STDERR_FILENO);
+        print(": not found\n", STDERR_FILENO);
+        g_status = 127;
+    }
+}
