69cdf1ad33079e26c3aeb284d2e19f987b66011e
JanefrancessC <janefrancesschioma@gmail.com>
2023-08-24 13:37:21 -0700

    run_shell

---
+++ run_shell.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+void initializer(char **current_command, int type_command)
+
+{
+
+    pid_t PID;
+
+
+
+    if (type_command == EXTERNAL_COMMAND || type_command == PATH_COMMAND)
+
+    {
+
+        PID = fork();
+
+        if (PID == 0)
+
+            execute_command(current_command, type_command);
+
+        else
+
+        {
+
+            waitpid(PID, &g_status, 0);
+
+            g_status >>= 8;
+
+        }
+
+    }
+
+    else
+
+        execute_command(current_command, type_command);
+
+}
